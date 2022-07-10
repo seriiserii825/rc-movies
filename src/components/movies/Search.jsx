5dee111a66f8523075fee4cb53ee162f9b237dfd
serii <seriiburduja@gmail.com>
2022-07-10 18:14:00 +0300

    feat: search refactor with hooks

--- src/components/movies/Search.jsx
+++ src/components/movies/Search.jsx
@@ -1,92 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Search extends React.Component {
-	state = {
-		search: '',
-		sticky: '',
-		type: 'all',
+function Search(props) {
+	const { searchMovies } = props;
+	const [search, setSearch] = useState('');
+	const [type, setType] = useState('all');
+	const [sticky, setSticky] = useState('');
+
+	const handleKey = (e) => {
+		if (e.key === 'Enter') {
+			searchMovies(search, type);
+			setSticky(search);
+		}
 	};
 
-	handleKey(e) {
-		if (e.key === 'Enter') {
-			this.props.searchMovies(this.state.search, this.state.type);
-			this.setState({ sticky: this.state.search });
-		}
-	}
+	const handleType = (e) => {
+		const typeData = e.target.dataset.type;
+		setType(typeData);
+		searchMovies(search, typeData);
+	};
 
-	handleType(e) {
-		this.setState(
-			() => ({ type: e.target.dataset.type }),
-			() => this.props.searchMovies(this.state.search, this.state.type)
-		);
-	}
-
-	render() {
-		return (
-			<div className='input-field'>
-				<div className='row'>
-					<h3>{this.state.sticky}</h3>
-				</div>
-				<div className='row'>
-					<input
-						placeholder='Search...'
-						value={this.state.search}
-						type='text'
-						className='validate col s10'
-						onChange={(e) =>
-							this.setState({ search: e.target.value })
-						}
-						onKeyDown={(e) => this.handleKey(e)}
-					/>
-					<button
-						className='btn col s2'
-						onClick={() =>
-							this.props.searchMovies(
-								this.state.search,
-								this.state.type
-							)
-						}>
-						Search
-					</button>
-				</div>
-				<div className='row filter'>
-					<label>
-						<input
-							className='with-gap'
-							name='type'
-							type='radio'
-							data-type='all'
-							onChange={(e) => this.handleType(e)}
-							checked={this.state.type === 'all'}
-						/>
-						<span>All</span>
-					</label>
-					<label>
-						<input
-							className='with-gap'
-							name='type'
-							type='radio'
-							data-type='movie'
-							onChange={(e) => this.handleType(e)}
-							checked={this.state.type === 'movie'}
-						/>
-						<span>Movie</span>
-					</label>
-					<label>
-						<input
-							className='with-gap'
-							name='type'
-							type='radio'
-							data-type='series'
-							onChange={(e) => this.handleType(e)}
-							checked={this.state.type === 'series'}
-						/>
-						<span>Series</span>
-					</label>
-				</div>
+	return (
+		<div className='input-field'>
+			<div className='row'>
+				<h3>{sticky}</h3>
 			</div>
-		);
-	}
+			<div className='row'>
+				<input
+					placeholder='Search...'
+					value={search}
+					type='text'
+					className='validate col s10'
+					onChange={(e) => setSearch(e.target.value)}
+					onKeyDown={(e) => handleKey(e)}
+				/>
+				<button
+					className='btn col s2'
+					onClick={() => searchMovies(search, type)}>
+					Search
+				</button>
+			</div>
+			<div className='row filter'>
+				<label>
+					<input
+						className='with-gap'
+						name='type'
+						type='radio'
+						data-type='all'
+						onChange={(e) => handleType(e)}
+						checked={type === 'all'}
+					/>
+					<span>All</span>
+				</label>
+				<label>
+					<input
+						className='with-gap'
+						name='type'
+						type='radio'
+						data-type='movie'
+						onChange={(e) => handleType(e)}
+						checked={type === 'movie'}
+					/>
+					<span>Movie</span>
+				</label>
+				<label>
+					<input
+						className='with-gap'
+						name='type'
+						type='radio'
+						data-type='series'
+						onChange={(e) => handleType(e)}
+						checked={type === 'series'}
+					/>
+					<span>Series</span>
+				</label>
+			</div>
+		</div>
+	);
 }
 
 export { Search };
